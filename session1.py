98871bd0758744a3b024fc2708d7b8cade833e2f
msa <msai.sadehi@gmail.com>
2023-07-02 14:56:44 +0330

    sadeghi

--- session1.py
+++ session1.py
@@ -1,1 +1,37 @@
-print("Hello every body")
+# print("Hello every body")
+# print(1)
+# print(1 + 2)
+# print(1 + 2 * 3)
+# print((1 + 2) * 3)
+
+# +  -  *   /    %  //
+
+# x = 0
+# print(x)
+# print(type(x))
+
+# x = "amir"
+# print(x)
+# print(type(x))
+
+# x = 1.4
+# print(x)
+#print(type(x))
+
+
+# number = 12
+# name = "sara"
+# name = 'sara'
+# name = ""
+# name = "a"
+
+
+name = "ali"
+print(name[0])
+print(name[1])
+print(name[2])
+
+# TODO   برنامه ای بنویسید که سه عدد را درون سه متغیر قرار دهد
+# سپس حاصل جمع، تفریق، تقسیم و ضرب ان ها محاسبه کنید و پرینت نمایید
+
+# TODO  برنامه ای بنویسید که اولین و آخرین کارکتر اسم شما را پرینت نماید
