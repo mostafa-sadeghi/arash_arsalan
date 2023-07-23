167f03a63d4581c5717b06d2ec42724b4c11d097
msa <msai.sadehi@gmail.com>
2023-07-23 15:00:37 +0330

    sadehgi

---
+++ session4.py
@@ -0,0 +1,23 @@
+# shopping_list = []
+# shopping_list.append("paper")
+# shopping_list.append("pen")
+# shopping_list.append("egg")
+# shopping_list.append("mashroom")
+# print(shopping_list)
+#
+# shopping_list.remove("egg")
+# print(shopping_list)
+
+# numbers = [1,2,3,4,77,80]
+
+# print(numbers[0])
+# print(numbers[1])
+#
+# print(numbers[0:2])
+# print(numbers[0:3])
+
+
+shopping_list = ['item1', "item2", "item3", "item4"]
+print(shopping_list[0])
+print(shopping_list[1])
+print(shopping_list[:3])
