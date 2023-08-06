b02b7877ec139523a36865bad0640b3810b1f879
msa <msai.sadehi@gmail.com>
2023-08-06 15:00:42 +0330

    t

---
+++ session5.py
@@ -0,0 +1,42 @@
+# numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+# my_tuple1 = ()
+# my_tuple2 = tuple()
+
+# print(numbers[0])
+# print(numbers[1])
+# print(numbers[2])
+
+# نوع داده تاپل مثل لیست است اما تغییرپذیر نیست
+
+
+# favorite_sports = {
+#     'nikan': 'football',
+#     "arash": 'basketball',
+#     'arsalan': 'footbal'
+# }
+
+# print(f"nikan likes {favorite_sports['nikan']}")
+# print(f"arsalan likes {favorite_sports['arsalan']}")
+# print(f"arash likes {favorite_sports['arash']}")
+
+# favorite_sports['arman'] = "tennis"
+# print(favorite_sports)
+
+# del favorite_sports['arman']
+# print(favorite_sports)
+
+# TODO
+"""
+برنامه ای بنویسید که نام و غذای مورد علاقه چهار نفر را از ووردی دریافت نماید و در یک دیکشنری ذخیره نماید
+"""
+
+import turtle
+
+my_screen = turtle.Screen()
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+my_turtle.shapesize(3)
+my_turtle.pensize(3)
+my_turtle.forward(100)
+
+turtle.done()
