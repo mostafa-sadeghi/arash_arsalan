6681c615c60df412eaf25c468a12d597347e4d91
msa <msai.sadeghi@gmail.com>
2023-10-23 17:57:34 +0330

    s

--- bagels.py
+++ bagels.py
@@ -14,4 +14,37 @@ def create_secret_number(num_digits):
     return ''.join(all_digits[:num_digits])
 
 
-print(create_secret_number(3))
+def check_guess(user_g, sec_num):
+    res = []
+    if user_g == sec_num:
+        return "you won"
+    for i in range(len(user_g)):
+        if user_g[i] == sec_num[i]:
+            res.append("Fermi")
+        elif user_g[i] in sec_num:
+            res.append("Pico")
+    if not len(res):
+        return "Bagels"
+    random.shuffle(res)
+    return " ".join(res)
+
+
+max_number = 10
+while True:
+    counter = 0
+    secret_number = create_secret_number(3)
+    print("secret number generated")
+    while counter < max_number:
+        print(f"guess number{counter + 1}")
+        user_guess = input("enter your guess: ")
+        print("user guess:", user_guess)
+
+        res = check_guess(user_guess, secret_number)
+        print(res)
+        if user_guess == secret_number:
+            break
+        counter += 1
+    
+    user_choice = input("Do you want to continue?  (y or n): ")
+    if user_choice == "n":
+        break
