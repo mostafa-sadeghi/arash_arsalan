3af887d03ba9516d2fb519ababd909f8a7e93d76
msa <msai.sadeghi@gmail.com>
2023-10-30 17:58:44 +0330

    s

--- exercise1.py
+++ exercise1.py
@@ -5,3 +5,31 @@ message = "salaam, khoobi?"
 با کمک هر کدام از حلقه ها فقط کاراکترهای الفبای انگلیسی را در متن بالا نمایش دهید
 
 """
+
+# for x in message:
+#     print(x)
+
+# for x in range(len(message)):
+#     print(message[x])
+
+
+# x = 0
+# while x < len(message):
+#     print(message[x])
+#     x += 1
+
+# message = "salaam, khoobi?"
+# for x in message:
+#     if x != "?" and x != " " and x != ",":
+#         print(x)
+
+
+my_list = [1, 2, 3, 4, 5, 6, "a", "b", "G", "*"]
+# print(len(my_list))
+
+for x in my_list:
+    if type(x) == int:
+        print(x)
+
+
+# TODO  از لیست بالا فقط اعداد را با هم جمع کنید و حاصل را نمایش دهید
